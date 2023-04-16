d7e60161fcd1f6271ebbcc585a477ea5e39832a8
piotrtabat <piotr.tabat1@gmail.com>
2023-04-16 11:44:48 +0200

    Footer - dodano element center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   background-color: #000;
@@ -18,6 +19,16 @@ const Left = styled.div`
   width: 100%;
   text-align: left;
 `
+const Center = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+  width: 100%;
+
+`
 const Right = styled.div`
   flex: 1;
   display: flex;
@@ -25,7 +36,6 @@ const Right = styled.div`
   justify-content: flex-end;
   margin-right: 3rem;
   width: 100%;
-
 `
 const Title = styled.h2`
   font-weight: bold;
@@ -46,11 +56,26 @@ const Line1 = styled.div`
   align-items: center;
   text-align: left;
   width: 5rem;
-  margin: 10px;
   height: 5rem;
+`
+const NavLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  &:hover {
+    color: #00a2ff;
+    text-decoration: underline;
+  }
 `
 
 const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
     return (
         <Container>
             <Left>
@@ -63,10 +88,21 @@ const Footer = () => {
                 <Line>Telefon: +48 123 456 789</Line>
                 <Line>E-mail: kontakt@pjoter.t-bat.com</Line>
             </Left>
+            <Center>
+                <NavLink to="/" onClick={scrollToTop}>
+                    Strona główna
+                </NavLink>
+                <NavLink to="/privacy">Polityka prywatności</NavLink>
+                <NavLink to="/regulations">Regulamin sklepu</NavLink>
+                <NavLink to="/payments">Płatności</NavLink>
+                <NavLink to="/delivery">Dostawa</NavLink>
+                <NavLink to="/returns">Zwroty</NavLink>
+                <NavLink to="/contact">Kontakt</NavLink>
+            </Center>
             <Right>
-                <Line1><a href="https://www.facebook.com/your_page" target="_blank" rel="noopener noreferrer"><FaFacebookF fontSize={40} /></a></Line1>
-                <Line1><a href="https://www.twitter.com/your_account" target="_blank" rel="noopener noreferrer"><FaTwitter fontSize={40} /></a></Line1>
-                <Line1><a href="https://www.instagram.com/your_account" target="_blank" rel="noopener noreferrer"><FaInstagram fontSize={40} /></a></Line1>
+                <Line1><a href="https://www.facebook.com/your_page" target="_blank" rel="noopener noreferrer"><FaFacebookF fontSize={30} /></a></Line1>
+                <Line1><a href="https://www.twitter.com/your_account" target="_blank" rel="noopener noreferrer"><FaTwitter fontSize={30} /></a></Line1>
+                <Line1><a href="https://www.instagram.com/your_account" target="_blank" rel="noopener noreferrer"><FaInstagram fontSize={30} /></a></Line1>
             </Right>
         </Container>
     )
