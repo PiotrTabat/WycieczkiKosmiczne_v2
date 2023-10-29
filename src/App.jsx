5c50ce9e820a535932ffa655647a9bd7e8378fbc
piotrtabat <piotr.tabat1@gmail.com>
2023-10-29 08:36:55 +0100

    Update build version

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-import {AnimatePresence} from 'framer-motion';
 import MainPage from './pages/MainPage';
-import {Routes, Route, useLocation} from 'react-router-dom';
 import Footer from './components/Footer';
 import Tour from './pages/Tour';
 import Accessory from './pages/Accessory';
 import AboutUs from './pages/AboutUs';
 import ShoppingCart from './pages/ShoppingCart';
-import Contact from "./pages/Contact";
-import Privacy from "./pages/Privacy";
-import Regulations from "./pages/Regulations";
-import Payments from "./pages/Payments";
-import Delivery from "./pages/Delivery";
-import Returns from "./pages/Returns";
-import Insurance from "./pages/Insurance";
+import Contact from './pages/Contact';
+import Privacy from './pages/Privacy';
+import Regulations from './pages/Regulations';
+import Payments from './pages/Payments';
+import Delivery from './pages/Delivery';
+import Returns from './pages/Returns';
+import Insurance from './pages/Insurance';
 import LoginToOrder from './components/LoginToOrder';
-import OrderPage from "./components/OrderPage";
-import {CartProvider} from "./components/CartContext";
-import LoginToUser from "./components/LoginToUser";
-import Register from "./components/Register";
+import OrderPage from './components/OrderPage';
+import { CartProvider } from './components/CartContext';
+import LoginToUser from './components/LoginToUser';
+import Register from './components/Register';
 
 const App = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -29,41 +28,35 @@ const App = () => {
         setIsOpen(!isOpen);
     };
 
-    const location = useLocation();
-
     return (
         <CartProvider>
-            <div>
-                <Navbar isOpen={isOpen} toggle={toggle}/>
-                <Sidebar isOpen={isOpen} toggle={toggle}/>
-                <AnimatePresence>
-                    <Routes location={location} key={location.pathname}>
-                        <Route
-                            path="/"
-                            element={<MainPage/>}
-                        />
-                        <Route path="tour/:id" element={<Tour/>}/>
-                        <Route path="/accessory/:id" element={<Accessory/>}/>
-                        <Route path="/insurance/:id" element={<Insurance/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                        <Route path="/shopping-cart" element={<ShoppingCart/>}/>
-                        <Route path="about-us" element={<AboutUs/>}/>
-                        <Route path="privacy" element={<Privacy/>}/>
-                        <Route path="regulations" element={<Regulations/>}/>
-                        <Route path="payments" element={<Payments/>}/>
-                        <Route path="delivery" element={<Delivery/>}/>
-                        <Route path="returns" element={<Returns/>}/>
-                        <Route path="login-to-order" element={<LoginToOrder/>}/>
-                        <Route path="login-to-user" element={<LoginToUser/>}/>
-                        <Route path="order" element={<OrderPage/>}/>
-                        <Route path="register" element={<Register/>}/>
+            <Router basename="/wycieczki-kosmiczne">
+                <div>
+                    <Navbar isOpen={isOpen} toggle={toggle} />
+                    <Sidebar isOpen={isOpen} toggle={toggle} />
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="tour/:id" element={<Tour />} />
+                        <Route path="/accessory/:id" element={<Accessory />} />
+                        <Route path="/insurance/:id" element={<Insurance />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/shopping-cart" element={<ShoppingCart />} />
+                        <Route path="about-us" element={<AboutUs />} />
+                        <Route path="privacy" element={<Privacy />} />
+                        <Route path="regulations" element={<Regulations />} />
+                        <Route path="payments" element={<Payments />} />
+                        <Route path="delivery" element={<Delivery />} />
+                        <Route path="returns" element={<Returns />} />
+                        <Route path="login-to-order" element={<LoginToOrder />} />
+                        <Route path="login-to-user" element={<LoginToUser />} />
+                        <Route path="order" element={<OrderPage />} />
+                        <Route path="register" element={<Register />} />
                     </Routes>
-                </AnimatePresence>
-                <Footer/>
-            </div>
+                    <Footer />
+                </div>
+            </Router>
         </CartProvider>
     );
 };
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import styled from 'styled-components';
-import { CartProvider } from './components/CartContext';
 
 const AppContainer = styled.div`
 `;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <AppContainer>
-            <CartProvider>
-            <App />
-            </CartProvider>
-        </AppContainer>
-    </BrowserRouter>
+    <AppContainer>
+        <App />
+    </AppContainer>
 );
-
